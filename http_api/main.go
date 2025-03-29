0cf1970fb92222ec31131760e0df744f3f54af95
hrithik210 <hrithiknayak7829@gmail.com>
2025-03-30 00:54:31 +0530

    wrote a post route

--- http_api/main.go
+++ http_api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -15,10 +16,33 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type postgreetReq struct {
+	Name string `json:"name"`
+}
+
+func PostGreet(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		var reqBody postgreetReq
+
+		err := json.NewDecoder(r.Body).Decode(&reqBody)
+
+		if err != nil {
+			http.Error(w, "invalid request", http.StatusBadRequest)
+		}
+		if reqBody.Name == "" {
+			http.Error(w, "name is required", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "Hello from post body %s", reqBody.Name)
+	} else {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.HandleFunc("/greet", HelloHandler)
+	http.HandleFunc("/api/postgreet", PostGreet)
 	fmt.Println("server is running on port 3000")
-	http.ListenAndServe(":3000", nil)
 	// Handle error properly
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
